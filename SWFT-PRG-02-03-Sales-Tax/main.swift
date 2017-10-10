195396d0a7d7244412c4b5f02ef694e712b0f7d2
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-10 18:22:59 -0400

    Initial version tested and working

--- SWFT-PRG-02-03-Sales-Tax/main.swift
+++ SWFT-PRG-02-03-Sales-Tax/main.swift
@@ -5,8 +5,18 @@
 //  Created by Keith Smith on 10/10/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that will compute the total sales tax
+//  on a $95 purchase. Assume the state sales tax is 4 percent,
+//  and the county sales tax is 2 percent.
 
 import Foundation
 
-print("Hello, World!")
+let taxState = 0.04
+let taxCounty = 0.02
+let purchase: Double = 95
 
+var totalCost: Double
+
+totalCost = (purchase * taxState) + (purchase * taxCounty) + purchase
+
+print(totalCost)
